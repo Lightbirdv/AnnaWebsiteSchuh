80aa0e019dbe2131b87954eaa34db19a5a4ff7b5
Lightbirdv <vicolino@gmx.de>
2021-05-29 20:55:15 +0200

    tablet version added

--- js/script.js
+++ js/script.js
@@ -71,3 +71,13 @@ var slider = new KeenSlider("#my-keen-slider4", {
   centered: true,
   loop: false,
 })
+
+var navTrigger = document.getElementsByClassName('nav-trigger')[0],
+    body = document.getElementsByTagName('body')[0];
+
+navTrigger.addEventListener('click', toggleNavigation);
+
+function toggleNavigation(event) {
+  event.preventDefault();
+  body.classList.toggle('nav-open');
+}
