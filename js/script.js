4ea83f80d65b9f68dd8689cf4217f77ebda55ae0
Lightbirdv <vicolino@gmx.de>
2021-05-29 21:18:26 +0200

    removed arrows on slider and >kinda< fixed dots

--- js/script.js
+++ js/script.js
@@ -1,26 +1,16 @@
 var slider = new KeenSlider("#my-keen-slider1", {
   created: function (instance) {
-    document
-      .getElementById("arrow-left")
-      .addEventListener("click", function () {
-        instance.prev();
-      });
-
-    document
-      .getElementById("arrow-right")
-      .addEventListener("click", function () {
-        instance.next();
-      });
     var dots_wrapper = document.getElementById("dots");
     var slides = document.querySelectorAll(".keen-slider__slide");
-    slides.forEach(function (t, idx) {
+    var index = 3;
+    for (idx = 0; idx < index; idx++) {
       var dot = document.createElement("button");
       dot.classList.add("dot");
       dots_wrapper.appendChild(dot);
       dot.addEventListener("click", function () {
         instance.moveToSlide(idx);
       });
-    });
+    };
     updateClasses(instance);
   },
   slideChanged(instance) {
@@ -30,14 +20,14 @@ var slider = new KeenSlider("#my-keen-slider1", {
 
 function updateClasses(instance) {
   var slide = instance.details().relativeSlide;
-  var arrowLeft = document.getElementById("arrow-left");
-  var arrowRight = document.getElementById("arrow-right");
-  slide === 0
-    ? arrowLeft.classList.add("arrow--disabled")
-    : arrowLeft.classList.remove("arrow--disabled");
-  slide === instance.details().size - 1
-    ? arrowRight.classList.add("arrow--disabled")
-    : arrowRight.classList.remove("arrow--disabled");
+  // var arrowLeft = document.getElementById("arrow-left");
+  // var arrowRight = document.getElementById("arrow-right");
+  // slide === 0
+  //   ? arrowLeft.classList.add("arrow--disabled")
+  //   : arrowLeft.classList.remove("arrow--disabled");
+  // slide === instance.details().size - 1
+  //   ? arrowRight.classList.add("arrow--disabled")
+  //   : arrowRight.classList.remove("arrow--disabled");
 
   var dots = document.querySelectorAll(".dot");
   dots.forEach(function (dot, idx) {
